ec8a93587cf73b8001c6c3266366fa11463eaf0f
Matt Gear <web_3_gears@protonmail.com>
2022-10-01 19:34:39 +0100

    Removing some legacy items, and minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,16 @@ function App() {
   return (
     <div className="App">
       <Navbar links={navbarLinks}/>
+      <div className='grid grid-rows-2 h-full w-full'>
+        <div className='grid grid-cols-2 h-8'>
+          <div className={'h-96 bg-amber-400'}></div>
+          <div className={'h-96 bg-green-400'}></div>
+        </div>
+        <div className='grid grid-cols-2'>
+          <div className={'h-96 bg-cyan-400'}></div>
+          <div className={'h-96 bg-purple-400'}></div>
+        </div>
+      </div>
     </div>
   );
 }
